152878da780b0238b3f87720f3ca5e00f0110611
XSolutionist <isachells92@hotmail.com>
2022-04-02 14:13:46 +0200

    Add project files.

---
+++ EventCallBackAndStateServiceDemo/Services/NotificationService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventCallBackAndStateServiceDemo.Services
+{
+    public class NotificationService : INotificationService
+    {
+        public int Count { get; set; } = 0;
+
+        public event Action OnChange;
+
+        public void IncrementCounter()
+        {
+            Count++;
+            OnChange?.Invoke();
+        }
+
+        public void ResetCounter()
+        {
+            Count = 0;
+            OnChange?.Invoke();
+        }
+    }
+    public interface INotificationService
+    {
+        event Action OnChange;
+        int Count { get; set; }
+        void IncrementCounter();
+        void ResetCounter();
+    }
+}
